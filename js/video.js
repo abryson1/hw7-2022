570caa1be37f60595a3b0ff5b04c3da9ce719de1
abryson1 <98368440+abryson1@users.noreply.github.com>
2022-11-11 16:40:07 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,47 +1,44 @@
 // var video = document.querySelector('#PlayerOne');
 
-var video = document.querySelector('#player1');; 
+var video = document.querySelector('#player1'); 
 
 window.addEventListener("load", function() {
 	video = document.querySelector('#player1')
 	console.log("Good job opening the window")
+	video.playbackRate = 1;
 	video.loop = false
 	video.autoplay = false;
+	console.log("Auto play is set to "+video.autoplay)
+	console.log("Loop is set to "+video.loop)
 });
 
 document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video")
+	console.log("Play Video");
+	document.querySelector("#volume").innerHTML= video.volume *100 + "%"
 	video.play()
 });
+
 
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video")
 	video.pause()
 });
 
+
 document.querySelector("#slower").addEventListener("click", function() {
-	console.log('Slow down video')
-	video.playbackeRate*=.90
-	console.log("Speed is", video.playbackeRate)
+	console.log("Slow down Video")
+	video.playbackeRate *= 0.9
+	console.log("Slow down Video", video.playbackeRate)
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
 	console.log('Speed up video')
-	video.playbackeRate/=1.1
+	video.playbackeRate /= .90
 	console.log("Speed is", video.playbackeRate)
 });
 
-
-document.querySelector("#slider").addEventListener("click", function() {
-	console.log('Speed up video', video.volume)
-	video.volume = this.value/100
-	console.log("current value is", video.volume)
-	console.log(document.querySelector("#slider"))
-});
-
 document.querySelector("#mute").addEventListener("click", function() {
-	// console.log('MUTED');
-	// video.muted = true;
+	console.log(this)
 	if (video.muted == true){
 		console.log('UNMUTED')
 		video.muted = false
@@ -52,6 +49,14 @@ document.querySelector("#mute").addEventListener("click", function() {
 		video.muted = true
 		this.innerHTML = 'Unmute'
 	}
+});
+
+
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log('current volume', video.volume)
+	video.volume = this.value/100
+	console.log("current value is", video.volume)
+	console.log(document.querySelector("#slider").innerHTML = video.volume *100 + "%")
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
@@ -71,62 +76,3 @@ document.querySelector("#vintage").addEventListener("click", function() {
 document.querySelector("#orig").addEventListener("click", function() {
 	video.classList.remove('oldSchool')
 });
-
-// document.querySelector("faster").addEventListener("click", function(){
-// 	video.playbackRate = 4;
-// 	console.log("Video speed", video.playbackeRate);
-// });
-// document.getElementById("faster").playbackRate = 1.5;
-
-
-// document.querySelector("#slower").addEventListener("click", function() {
-// 	console.log('Slow down video');
-// 	video.playbackeRate*= .90;
-// 	video.play();
-// 	console.log("Speed is", video.playbackeRate)
-
-// });
-
-
-
-// document.querySelector("#p").addEventListener("click", function() {
-// 	pauseButton.disabled = false;
-// });
-
-
-
-
-// document.querySelector("#slower").addEventListener("click", function() {
-// 	console.log('Slow down video');
-// 	video.playbackeRate*=0.90
-// 	console.log("Speed is", video.playbackeRate)
-// });
-
-// document.querySelector("#faster").addEventListener("click", function() {
-// 	console.log('Speed up video');
-// 	video.playbackeRate/=1.1
-// 	console.log("Speed is", video.playbackeRate)
-// });
-
-// document.querySelector("#SkipAhead").addEventListener("click", function() {
-// 	console.log('Speed up video');
-// 	video.playbackeRate/=1.1
-// 	console.log("Speed is", video.playbackeRate)
-// });
-
-
-// document.querySelector("#slider").addEventListener("click", function() {
-// 	console.log('Speed up video', video.volume);
-// 	video.volume = this.value/100
-// 	console.log("current value is", video.volume)
-// 	console.log(document.querySelector("#volume")
-// });
-
-// document.querySelector("#volume").innerHTML({
-// 	console.log('Speed up video');
-// 	video.playbackeRate*=1.1
-// 	console.log("Speed is", video.playbackeRate)
-// });
-// // 
-// // doc
-
