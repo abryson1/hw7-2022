abe41d73fa1ce743d7be0d9b443e655a08852ee7
abryson1 <98368440+abryson1@users.noreply.github.com>
2022-11-11 19:04:19 -0500

    Add files via upload

--- video.js
+++ video.js
@@ -1,20 +1,17 @@
-// var video = document.querySelector('#PlayerOne');
-
-var video = document.querySelector('#player1'); 
+var video = document.querySelector('#player1');
 
 window.addEventListener("load", function() {
-	video = document.querySelector('#player1')
 	console.log("Good job opening the window")
-	video.playbackRate = 1;
-	video.loop = false
 	video.autoplay = false;
+	video.loop = false
 	console.log("Auto play is set to "+video.autoplay)
 	console.log("Loop is set to "+video.loop)
 });
 
+
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
-	document.querySelector("#volume").innerHTML= video.volume *100 + "%"
+	document.querySelector("#volume").innerHTML= video.volume * 100 + "%"
 	video.play()
 });
 
@@ -24,18 +21,18 @@ document.querySelector("#pause").addEventListener("click", function() {
 	video.pause()
 });
 
-
 document.querySelector("#slower").addEventListener("click", function() {
-	console.log("Slow down Video")
-	video.playbackeRate *= 0.9
-	console.log("Slow down Video", video.playbackeRate)
+	console.log("Slow Down")
+	video.playbackRate *= 0.9
+	console.log("Speed is " + video.playbackRate)
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	console.log('Speed up video')
-	video.playbackeRate /= .90
-	console.log("Speed is", video.playbackeRate)
+	console.log("Speed up")
+	video.playbackRate /= 0.9
+	console.log("Speed is " + video.playbackRate)
 });
+
 
 document.querySelector("#mute").addEventListener("click", function() {
 	console.log(this)
@@ -56,7 +53,7 @@ document.querySelector("#slider").addEventListener("click", function() {
 	console.log('current volume', video.volume)
 	video.volume = this.value/100
 	console.log("current value is", video.volume)
-	console.log(document.querySelector("#slider").innerHTML = video.volume *100 + "%")
+	document.querySelector("#volume").innerHTML = video.volume *100 + "%"
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
@@ -76,3 +73,4 @@ document.querySelector("#vintage").addEventListener("click", function() {
 document.querySelector("#orig").addEventListener("click", function() {
 	video.classList.remove('oldSchool')
 });
+
